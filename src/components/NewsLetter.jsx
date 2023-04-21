9ca54ca38f6e00995d2a2974fd59c7022d1ce074
Gary Buckle <buckle.g@mac.com>
2023-04-21 14:45:37 +0000

    Form submission complete

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -5,44 +5,62 @@ function NewsLetter() {
   const [newsData, setNewsData] = useState({
     newsTitle: '',
     newsText: '',
+    newsSubmitted: false,
   });
   const changeNewsTitleHandler = (event) => {
     setNewsData({
       newsTitle: event.target.value,
       newsText: newsData.newsText,
+      newsSubmitted: false,
     });
   };
   const changeNewsTextHandler = (event) => {
     setNewsData({
       newsTitle: newsData.newsTitle,
       newsText: event.target.value,
+      newsSubmitted: false,
     });
   };
 
   const resetNewsFields = () => {
     setNewsData({ newsTitle: '', newsText: '' });
   };
+
+  const submitHandler = () => {
+    event.preventDefault();
+    setNewsData({
+      newsTitle: newsData.newsTitle,
+      newsText: newsData.newsText,
+      newsSubmitted: true,
+    });
+    console.log('Form Submoitted');
+  };
+
   return (
     <div className="newsletter">
       <div className="title">Newsletter</div>
-      <input
-        value={newsData.newsTitle}
-        type="text"
-        placeholder=" News Headline"
-        onChange={changeNewsTitleHandler}
-      />
-      <p>{newsData.newsTitle.length} Characters</p>
-      <input
-        value={newsData.newsText}
-        type="text"
-        placeholder=" Main News Text"
-        onChange={changeNewsTextHandler}
-      />
-      {newsData.newsText.length} Characters
-      <div>
-        <button type="submit">Submit</button>
-        <button onClick={resetNewsFields}>Clear Fields</button>
-      </div>
+      <form onSubmit={submitHandler}>
+        {' '}
+        <input
+          value={newsData.newsTitle}
+          type="text"
+          placeholder=" News Headline"
+          onChange={changeNewsTitleHandler}
+        />
+        <p>{newsData.newsTitle.length} Characters</p>
+        <input
+          value={newsData.newsText}
+          type="text"
+          placeholder=" Main News Text"
+          onChange={changeNewsTextHandler}
+        />
+        {newsData.newsText.length} Characters
+        <div>
+          <button type="submit">Submit</button>
+          <button onClick={resetNewsFields}>Clear Fields</button>
+        </div>
+      </form>
+      {newsData.newsSubmitted ? <p>Form Submitted</p> : <p>Not Submitted</p>}
     </div>
   );
 }
