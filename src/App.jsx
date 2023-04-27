0bfa606ee83e796d346ff5a4f9c00a592fcf4a28
Gary Buckle <buckle.g@mac.com>
2023-04-27 14:19:32 +0000

    tailwind not working!

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>garybuckle adventures with Vite + React</h1>
+      <h1 className="text-3xl font-bold underline">
+        garybuckle adventures with Vite + React
+      </h1>
       <div>
         <button onClick={clearSreenHandler}>
           {clearScreen ? 'Show Screen' : 'Clear Screen'}
         </button>
+        <button className="rounded-full"> TailWindCSS</button>
       </div>
       {!clearScreen ? (
         <div className="small-container">
